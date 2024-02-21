45547a4b042585a819425edc30bc56b1be3789c2
Subhajit Pramanik <subhajitpramanik1011@gmail.com>
2024-02-21 11:43:24 +0530

    typo mistake

--- backend/src/utils/ApiResponse.js
+++ backend/src/utils/ApiResponse.js
@@ -1,6 +1,6 @@
 class ApiResponse {
-    constructor(status, data, message = "Success") {
-        this.status = status;
+    constructor(statusCode, data, message = "Success") {
+        this.statusCode = statusCode;
         this.data = data;
         this.message = message;
         this.success = statusCode < 400;
