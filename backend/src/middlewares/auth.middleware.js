3af9057468fd21a44e2e8216ac73a9ca4b1df86a
Subhajit Pramanik <subhajitpramanik1011@gmail.com>
2024-02-25 00:20:12 +0530

    fix apierror

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,16 +1,17 @@
 import { User } from "../models/user.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
 import jwt from "jsonwebtoken";
 
 export const verifyJWT = asyncHandler(async (req, _, next) => {
     /*
-    * data from request header
-    * Authorization: Bearer <token>
-    * verify token from request header
-    * get user
-    * return
-    * next
-    */
+     * data from request header
+     * Authorization: Bearer <token>
+     * verify token from request header
+     * get user
+     * return
+     * next
+     */
 
     try {
         const token =
@@ -18,10 +19,11 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
             req.header("Authorization")?.replace("Bearer ", "");
 
         if (!token) {
-            return next(new ApiError(401, "No token provided"));
+            throw new ApiError(401, "No token provided");
         }
 
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
 
         const user = await User.findById(decodedToken?._id).select(
             "-password -refreshToken"
