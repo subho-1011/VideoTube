1dd266c8ff0b65224b89bd5e0eb8eda4ebe40d2c
Subhajit Pramanik <subhajitpramanik1011@gmail.com>
2024-02-25 22:34:56 +0530

    typo

--- backend/src/models/video.model.js
+++ backend/src/models/video.model.js
@@ -1,43 +1,43 @@
 import mongoose, { Schema } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-const video = new Schema(
-  {
-    VideoFile: {
-      type: String,
-      required: true,
+const videoSchema = new Schema(
+    {
+        videoFile: {
+            type: String,
+            required: true,
+        },
+        thumbnail: {
+            type: String,
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        duration: {
+            type: Number,
+            required: true,
+        },
+        views: {
+            type: Number,
+            default: 0,
+        },
+        isPulished: {
+            type: Boolean,
+            default: true,
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
     },
-    thumbnail: {
-      type: String,
-      required: true,
-    },
-    title: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    duration: {
-      type: Number,
-      required: true,
-    },
-    views: {
-      type: Number,
-      default: 0,
-    },
-    isPulished: {
-      type: Boolean,
-      default: true,
-    },
-    owner: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 videoSchema.plugin(mongooseAggregatePaginate);
