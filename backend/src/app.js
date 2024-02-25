cfaee8f1bc5adf06e08b8383da32620d063ce7c1
Subhajit Pramanik <subhajitpramanik1011@gmail.com>
2024-02-25 22:36:42 +0530

    add video routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,9 +19,11 @@ app.use(cookieParser());
 
 // routes
 import userRouter from "./routes/user.routes.js";
+import videoRouter from "./routes/video.routes.js";
 
 // routes declaretion
 // http://localhost:8000/api/v1/users/register
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/videos", videoRouter);
 
 export { app };
---
+++ backend/src/routes/video.routes.js
@@ -0,0 +1,26 @@
+import { Router } from "express";
+
+import {
+    publishAVideo,
+    getVideoById,
+    updateVideo,
+    deleteVideo
+} from "../controllers/video.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+router.use(verifyJWT);
+
+router.route("/publish").post(
+    upload.fields([
+        { name: "videoFile", maxCount: 1 },
+        { name: "thumbnail", maxCount: 1 },
+    ]),
+    publishAVideo
+);
+router.route("/:videoId").get(getVideoById);
+router.route("/:videoId").patch(upload.single("thumbnail"), updateVideo);
+router.route("/:videoId").delete(deleteVideo);
+
+export default router;
