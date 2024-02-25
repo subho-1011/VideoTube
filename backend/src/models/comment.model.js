473ca81907b7a282f5b8c33cf5c87482c6c2b9e3
Subhajit Pramanik <subhajitpramanik1011@gmail.com>
2024-02-25 12:07:41 +0530

    add pagiante plugin

--- backend/src/models/comment.model.js
+++ backend/src/models/comment.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const commenstSchema = new Schema(
     {
@@ -9,15 +10,14 @@ const commenstSchema = new Schema(
         video: {
             type: Schema.Types.ObjectId,
             ref: "Video",
-            required: true,
         },
         owner: {
             type: Schema.Types.ObjectId,
             ref: "User",
-            required: true,
         },
     },
     { timestamps: true }
 );
 
+commentSchema.plugin(mongooseAggregatePaginate)
 export const Comment = mongoose.model("Comment", commenstSchema);
