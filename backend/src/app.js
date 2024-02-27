d21c74c409bccfd12c95f2fe26afba9bf6b4000f
Subhajit Pramanik <subhajitpramanik1011@gmail.com>
2024-02-27 17:59:34 +0530

    add playlist controller & routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -25,6 +25,7 @@ import healthCheckRouter from "./routes/healthcheck.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import likeRouter from "./routes/like.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
 
 // routes declaretion
 // http://localhost:8000/api/v1/users/register
@@ -35,5 +36,6 @@ app.use("/api/v1/health-check", healthCheckRouter);
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/playlists", playlistRouter);
 
 export { app };
